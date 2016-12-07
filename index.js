a0633fcb6de4b6f53781c50e2bf736ad231772e9
Fabio Zuber <fabio.zuber92@gmail.com>
2016-12-07 16:33:26 +0100

    feat: add /start handling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const TextCommand = Telegram.TextCommand;
 const request = require('request');
 const cheerio = require('cheerio');
 
-class MenuController extends TelegramBaseController {
+class MealsController extends TelegramBaseController {
 
     parse(url, callback) {
         const baseURL = 'http://siemens.sv-restaurant.ch/de/';
@@ -21,23 +21,39 @@ class MenuController extends TelegramBaseController {
                 text += $(offer).find('.menu-description .trimmings').text() + '\n';
                 text += '_' + $(offer).find('.price .price-item').text() + '_\n\n'
             });
+
+            text = text.replace(/`/g, '' );
             callback(text)
         });
     }
 
-    menuHandler($) {
+    mealsHandler($) {
         const self = this;
         var response = '';
         self.parse('menuplan.html', function(text) {
             response += text;
-            response = response.replace(/`/g, '' );
             $.sendMessage(response, { parse_mode: 'Markdown'});
         })
     }
 
     get routes() {
         return {
-            'getToday': 'menuHandler'
+            'getToday': 'mealsHandler'
+        }
+    }
+}
+
+class StartController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    start($) {
+        $.sendMessage('Hello! \n I can send you the menu for the SV restaurant in Zug. \n Try /get or /getDaily (for daily updates)')
+    }
+
+    get routes() {
+        return {
+            'startHandler': 'start'
         }
     }
 }
@@ -45,5 +61,9 @@ class MenuController extends TelegramBaseController {
 tg.router
     .when(
         new TextCommand('get', 'getToday'),
-        new MenuController()
+        new MealsController()
+    )
+    .when(
+        new TextCommand('/start', 'startHandler'),
+        new StartController()
     );
