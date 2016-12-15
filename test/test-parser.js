59a2eed56e4ee53caf0849798b96b9277fd81c64
Fabio Zuber <fabio.zuber92@gmail.com>
2016-12-15 10:25:24 +0100

    test: read menuplan html localy

--- test/test-parser.js
+++ test/test-parser.js
@@ -1,14 +1,20 @@
 'use strict';
 const assert = require('chai').assert;
+const fs = require('fs');
+const cheerio = require("cheerio/lib/static.js");
 const MealsController = require('../app/app');
 
+
 describe('The sv page parer module', function(){
-    it('parses the menuplan page of 2016-02-14',function () {
-        var mealsController = new MealsController.MealsController();
-        console.log("bla")
-        mealsController.parse('http://siemens.sv-restaurant.ch/de/menuplan.html',function (text) {
-            console.log("callback");
-            console.log(text);
+    it('formats the menuplan page of 2016-02-14 correctly',function () {
+        let menuplanPath = __dirname + "\\menuplan2016-02-14.html";
+
+        let mealsController = new MealsController.MealsController();
+
+        let $ = cheerio.load(fs.readFileSync(menuplanPath));
+        mealsController.formatMessage($,function (text) {
+            console.warn("callback");
+            console.warn(text);
         });
 /*        mealsController.parse('file://__dirname\\test\Menuplan2016-02-14.html',function () {
 
