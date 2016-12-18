115feee6dced7e49a949679c4bb4d9c3a965cab5
Fabio Zuber <fabio.zuber92@gmail.com>
2016-12-18 18:34:13 +0100

    feat: allow people to subscribe and remove themself for daily updates :watch:

--- app/app.js
+++ app/app.js
@@ -3,11 +3,14 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot_api_token = process.env.BOT_API_TOKEN;
 const tgBot  = new TelegramBot(bot_api_token, { polling: true });
 const Parser = require('./SVPageParser');
+const Subscriptions = require('./subscriptions');
 
 /* Routes */
-tgBot.onText(/\/get(Today)?/,getTodayHandler);
+tgBot.onText(/\/get(Today)?$/mg,getTodayHandler);
 tgBot.onText(/\/getDaily/,getDailyHandler);
 tgBot.onText(/\/start/,startHandler);
+tgBot.onText(/\/stop/,cancelSubscriptionsHandler);
+tgBot.onText(/\/cancel/,cancelSubscriptionsHandler);
 
 
 /* Handlers */
@@ -22,8 +25,27 @@ function getTodayHandler(message) {
 }
 
 function getDailyHandler(message) {
+    let chat = message.chat;
     let chatId = message.chat.id;
 
+    const subscriptions = new Subscriptions();
+    subscriptions.add(chat,function () {
+        let markdownText = "*Successfully added you to the daily subscriber list* \n" +
+            "I will send you the menu at 10:00am from now on.";
+        tgBot.sendMessage(chatId,markdownText,{ parse_mode: 'Markdown'});
+    });
+}
+
+function cancelSubscriptionsHandler(message) {
+    let chat = message.chat;
+    let chatId = message.chat.id;
+
+    const subscriptions = new Subscriptions();
+    subscriptions.remove(chat,function () {
+        let markdownText = "*Successfully removed you from the daily subscriber list* \n" +
+            "I will no longer bother you";
+        tgBot.sendMessage(chatId,markdownText,{ parse_mode: 'Markdown'});
+    });
 }
 
 function startHandler(message) {
---
+++ app/subscriptions.js
@@ -0,0 +1,50 @@
+'use strict';
+var mongodb_uri = process.env.MONGODB_URI;
+var MongoClient = require('mongodb').MongoClient,
+    co = require('co'),
+    assert = require('assert');
+
+class Subscriptions{
+
+    add(chat,callback){
+        co(function*() {
+            //connect to db
+            var db = yield MongoClient.connect(mongodb_uri);
+            console.log("Connected correctly to server");
+
+            // Insert a single document
+            yield db.collection('subscribers').updateOne(
+                {id:chat.id},
+                {$set:{'firstname': chat.first_name, 'lastname': chat.last_name, 'type': chat.type, 'username': chat.username }},
+                { upsert: true}
+            );
+
+            // Close connection
+            db.close();
+        }).catch(function(err) {
+            console.log(err.stack);
+        });
+
+        callback();
+    }
+
+    remove(chat, callback){
+        co(function*() {
+            //connect to db
+            var db = yield MongoClient.connect(mongodb_uri);
+            console.log("Connected correctly to server");
+
+            // delete a single document
+            yield db.collection('subscribers').removeOne({id:chat.id});
+
+            // Close connection
+            db.close();
+        }).catch(function(err) {
+            console.log(err.stack);
+        });
+
+        callback();
+    }
+}
+
+module.exports = Subscriptions;
