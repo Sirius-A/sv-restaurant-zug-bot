c8b288e620d6abaf8635a6049950437be51e685f
Fabio Zuber <fabio.zuber92@gmail.com>
2017-10-11 21:15:49 +0200

    feat(parser): send remaining weekdays

--- app/SVPageParser.js
+++ app/SVPageParser.js
@@ -3,6 +3,8 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 class SVPageParser{
+
+
     parseToday(url, callback) {
         const self = this;
         request(url, function(err, resp, body){
@@ -22,27 +24,15 @@ class SVPageParser{
     }
 
     formatWeek($) {
-        let offers = $('#menu-plan-tab1').find('.menu-item');
+        let days = $('.day-nav').find('li');
+
         let text = '';
-        $(offers).each(function (i, offer) {
-            let menuDescription = $(offer).find('.menu-description');
-            menuDescription.find('br').replaceWith(' ');
+        $(days).each(function (i, day) {
+            let dayName = $(day).find('.day').text();
+            let dayDate= $(day).find('.date').text();
 
-            text += '*' + removeSpecialCharacters($(offer).find('.menu-title').text().trim()) + '*\n';
-            text += removeSpecialCharacters(menuDescription.text()) + '\n';
-            text += '_' + removeSpecialCharacters($(offer).find('.prices-3 .val').text() + ' ' + $(offer).find('.prices-3 .desc').text()) + "_\n";
-            let provenance = removeSpecialCharacters($(offer).find('.menu-provenance').text());
-            if (provenance.length > 1) {
-                text += '_' + provenance + "_\n\n";
-            } else {
-                text += "\n";
-            }
-
+            text += `*${dayName} ${dayDate} \n*`
         });
-
-        function removeSpecialCharacters(text) {
-            return (text.replace(/`|\*|_|#|\r/g, ''));
-        }
 
         return text;
     }
