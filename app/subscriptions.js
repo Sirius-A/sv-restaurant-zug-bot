e87c7708aa96075cf3fc6c337300cc000c904dd6
Fabio Zuber <fabio.zuber92@gmail.com>
2018-10-10 21:21:19 +0200

    style: change indent to 2 and add editorconfig

--- app/subscriptions.js
+++ app/subscriptions.js
@@ -1,120 +1,120 @@
 'use strict';
 const mongodb_uri = process.env.MONGODB_URI;
 const MongoClient = require('mongodb').MongoClient,
-      co = require('co');
+  co = require('co');
 
 class Subscriptions{
 
-    add(chat,callback){
-        co(function*() {
-            //connect to db
-            const db = yield MongoClient.connect(mongodb_uri);
-            console.log("Connected correctly to server");
+  add(chat,callback){
+    co(function*() {
+      //connect to db
+      const db = yield MongoClient.connect(mongodb_uri);
+      console.log("Connected correctly to server");
 
-            // Insert/update a single document
-            yield db.collection('subscribers').updateOne(
-                {id:chat.id},
-                {$set:{'firstname': chat.first_name, 'lastname': chat.last_name, 'type': chat.type, 'username': chat.username }},
-                { upsert: true}
-            );
+      // Insert/update a single document
+      yield db.collection('subscribers').updateOne(
+        {id:chat.id},
+        {$set:{'firstname': chat.first_name, 'lastname': chat.last_name, 'type': chat.type, 'username': chat.username }},
+        { upsert: true}
+      );
 
-            // Close connection
-            db.close();
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+      // Close connection
+      db.close();
+    }).catch(function(err) {
+      console.log(err.stack);
+    });
 
-        callback();
+    callback();
+  }
+
+  addWeekday(chat, weekday, callback){
+    co(function*() {
+      //connect to db
+      const db = yield MongoClient.connect(mongodb_uri);
+      console.log("Connected correctly to server");
+
+      // Insert/update a single document
+      yield db.collection('subscribers').updateOne(
+        {id:chat.id},
+        {$addToSet: {weekdays: weekday}},
+        {upsert: true}
+      );
+
+      // Close connection
+      db.close();
+    }).catch(function(err) {
+      console.log(err.stack);
+    });
+
+    callback();
+  }
+
+  remove(chatId, callback){
+    co(function*() {
+      //connect to db
+      const db = yield MongoClient.connect(mongodb_uri);
+      console.log("Connected correctly to server");
+
+      // delete a single document
+      yield db.collection('subscribers').removeOne({id:chatId});
+
+      // Close connection
+      db.close();
+    }).catch(function(err) {
+      console.log(err.stack);
+    });
+    if(typeof callback === 'function') {
+      callback();
     }
+  }
 
-    addWeekday(chat, weekday, callback){
-        co(function*() {
-            //connect to db
-            const db = yield MongoClient.connect(mongodb_uri);
-            console.log("Connected correctly to server");
+  getWeekdays(chat,callback){
+    co(function*() {
+      //connect to db
+      const db = yield MongoClient.connect(mongodb_uri);
+      console.log("Connected correctly to server");
 
-            // Insert/update a single document
-            yield db.collection('subscribers').updateOne(
-                {id:chat.id},
-                {$addToSet: {weekdays: weekday}},
-                {upsert: true}
-            );
+      // Insert/update a single document
+      db.collection('subscribers').findOne({ $and: [ {id:chat.id}, {weekdays:{$exists: true}}]},callback);
 
-            // Close connection
-            db.close();
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+      // Close connection
+      db.close();
+    }).catch(function(err) {
+      console.log(err.stack);
+    });
 
-        callback();
-    }
+  }
 
-    remove(chatId, callback){
-        co(function*() {
-            //connect to db
-            const db = yield MongoClient.connect(mongodb_uri);
-            console.log("Connected correctly to server");
+  forAllDailly(next){
+    co(function*() {
+      //connect to db
+      const db = yield MongoClient.connect(mongodb_uri);
+      console.log("Connected correctly to server");
 
-            // delete a single document
-            yield db.collection('subscribers').removeOne({id:chatId});
+      //find all daily subs
+      db.collection('subscribers').find({weekdays:{$exists: false}}).forEach(next);
 
-            // Close connection
-            db.close();
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
-        if(typeof callback === 'function') {
-            callback();
-        }
-    }
+      // Close connection
+      db.close();
+    }).catch(function(err) {
+      console.log(err.stack);
+    });
+  }
+  forAllParttime(weekdayIndex,next){
+    co(function*() {
+      //connect to db
+      const db = yield MongoClient.connect(mongodb_uri);
+      console.log("Connected correctly to server");
 
-    getWeekdays(chat,callback){
-        co(function*() {
-            //connect to db
-            const db = yield MongoClient.connect(mongodb_uri);
-            console.log("Connected correctly to server");
+      // find all documents containing a given weekday
+      db.collection('subscribers').find({weekdays: weekdayIndex}).forEach(next);
 
-            // Insert/update a single document
-            db.collection('subscribers').findOne({ $and: [ {id:chat.id}, {weekdays:{$exists: true}}]},callback);
-
-            // Close connection
-            db.close();
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
-
-    }
-
-    forAllDailly(next){
-        co(function*() {
-            //connect to db
-            const db = yield MongoClient.connect(mongodb_uri);
-            console.log("Connected correctly to server");
-
-            //find all daily subs
-            db.collection('subscribers').find({weekdays:{$exists: false}}).forEach(next);
-
-            // Close connection
-            db.close();
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
-    }
-    forAllParttime(weekdayIndex,next){
-        co(function*() {
-            //connect to db
-            const db = yield MongoClient.connect(mongodb_uri);
-            console.log("Connected correctly to server");
-
-            // find all documents containing a given weekday
-            db.collection('subscribers').find({weekdays: weekdayIndex}).forEach(next);
-
-            // Close connection
-            db.close();
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
-    }
+      // Close connection
+      db.close();
+    }).catch(function(err) {
+      console.log(err.stack);
+    });
+  }
 }
 
 module.exports = Subscriptions;
