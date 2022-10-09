7f49f724ed948f8083e684d137cf96d6acb5c62f
Fabio Zuber <fabio.zuber@siemens.com>
2022-10-09 17:58:15 +0200

    feat(svParser): add location to message

--- app/SVPageParser.js
+++ app/SVPageParser.js
@@ -45,7 +45,8 @@ class SVPageParser{
       let menuDescription = $(offer).find('.menu-description');
       menuDescription.find('br').replaceWith(' ');
 
-      text += '*' + removeSpecialCharacters($(offer).find('.menu-title').text().trim()) + '*\n';
+      text += '*' + removeSpecialCharacters($(offer).find('.menu-title').text().trim()) + '*';
+      text += '_(' + removeSpecialCharacters($(offer).find('.menuline').text()) + ')_\n';
       text += removeSpecialCharacters(menuDescription.text()) + '\n';
       text += '_' + removeSpecialCharacters($(offer).find('.prices-3 .val').text() + ' ' + $(offer).find('.prices-3 .desc').text()) + "_\n";
       let provenance = removeSpecialCharacters($(offer).find('.menu-provenance').text());
