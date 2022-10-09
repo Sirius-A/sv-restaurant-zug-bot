11d3ba9f9d11f5ea06142fc4f3248d2d2f52fdaf
Fabio Zuber <fabio.zuber@siemens.com>
2022-10-09 22:18:37 +0200

    style: format code to eslint rules

--- app/SVPageParser.js
+++ app/SVPageParser.js
@@ -3,33 +3,32 @@ const request = require('request');
 const cheerio = require('cheerio');
 const url = 'http://siemens.sv-restaurant.ch/de/menuplan/';
 
-class SVPageParser{
-
+class SVPageParser {
   parseToday(callback) {
     request(url, (err, resp, body) => {
-      let $ = cheerio.load(body);
-      let offers = $('#menu-plan-tab1').find('.menu-item');
-      let message = this.formatDayMenu($, offers);
+      const $ = cheerio.load(body);
+      const offers = $('#menu-plan-tab1').find('.menu-item');
+      const message = this.formatDayMenu($, offers);
       callback(message);
     });
   }
 
   parseWeek(callback) {
     request(url, (err, resp, body) => {
-      let $ = cheerio.load(body);
-      let message = this.formatWeek($);
+      const $ = cheerio.load(body);
+      const message = this.formatWeek($);
       callback(message);
     });
   }
 
   formatWeek($) {
-    let days = $('.day-nav').find('li');
+    const days = $('.day-nav').find('li');
 
     let text = '';
     $(days).each((i, day) => {
-      let dayName = $(day).find('.day').text();
-      let dayDate= $(day).find('.date').text();
-      let offers = $(`#menu-plan-tab${i+1}`).find('.menu-item');
+      const dayName = $(day).find('.day').text();
+      const dayDate= $(day).find('.date').text();
+      const offers = $(`#menu-plan-tab${i+1}`).find('.menu-item');
 
       text += `*${dayName} ${dayDate}* \n`;
       text += `========\n`;
@@ -41,19 +40,19 @@ class SVPageParser{
 
   formatDayMenu($, offers) {
     let text = '';
-    $(offers).each(function (i, offer) {
-      let menuDescription = $(offer).find('.menu-description');
+    $(offers).each(function(i, offer) {
+      const menuDescription = $(offer).find('.menu-description');
       menuDescription.find('br').replaceWith(' ');
 
       text += '_' + removeSpecialCharacters($(offer).find('.menuline').text()) + '_\n';
       text += '*' + removeSpecialCharacters($(offer).find('.menu-title').text().trim()) + '*\n';
       text += removeSpecialCharacters(menuDescription.text()) + '\n';
-      text += '_' + removeSpecialCharacters($(offer).find('.prices-3 .val').text() + ' ' + $(offer).find('.prices-3 .desc').text()) + "_\n";
-      let provenance = removeSpecialCharacters($(offer).find('.menu-provenance').text());
+      text += '_' + removeSpecialCharacters($(offer).find('.prices-3 .val').text() + ' ' + $(offer).find('.prices-3 .desc').text()) + '_\n';
+      const provenance = removeSpecialCharacters($(offer).find('.menu-provenance').text());
       if (provenance.length > 1) {
-        text += '_' + provenance + "_\n\n";
+        text += '_' + provenance + '_\n\n';
       } else {
-        text += "\n";
+        text += '\n';
       }
     });
 
--- app/subscriptions.js
+++ app/subscriptions.js
@@ -1,15 +1,15 @@
-const MongoClient = require('mongodb').MongoClient
-const mongodb_uri = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const mongodbUri = process.env.MONGODB_URI;
 
-class Subscriptions{
-  async add(chat,callback){
-    const client = new MongoClient(mongodb_uri);
+class Subscriptions {
+  async add(chat, callback) {
+    const client = new MongoClient(mongodbUri);
     try {
       const db = client.db('sv-bot');
       await db.collection('subscribers').updateOne(
-        {id:chat.id},
-        {$set:{'firstname': chat.first_name, 'lastname': chat.last_name, 'type': chat.type, 'username': chat.username }},
-        { upsert: true}
+          {id: chat.id},
+          {$set: {'firstname': chat.first_name, 'lastname': chat.last_name, 'type': chat.type, 'username': chat.username}},
+          {upsert: true},
       );
     } finally {
       await client.close();
@@ -18,16 +18,16 @@ class Subscriptions{
     callback();
   }
 
-  async addWeekday(chat, weekday, callback){
-    const client = new MongoClient(mongodb_uri);
+  async addWeekday(chat, weekday, callback) {
+    const client = new MongoClient(mongodbUri);
     try {
       const db = client.db('sv-bot');
 
       // Insert/update a single document
       await db.collection('subscribers').updateOne(
-        {id:chat.id},
-        {$addToSet: {weekdays: weekday}},
-        {upsert: true}
+          {id: chat.id},
+          {$addToSet: {weekdays: weekday}},
+          {upsert: true},
       );
     } finally {
       // Ensures that the client will close when you finish/error
@@ -36,61 +36,56 @@ class Subscriptions{
     callback();
   }
 
-  async remove(chatId, callback){
-    const client = new MongoClient(mongodb_uri);
+  async remove(chatId, callback) {
+    const client = new MongoClient(mongodbUri);
     try {
       const db = client.db('sv-bot');
 
       // delete a single document
-      await db.collection('subscribers').deleteOne({id:chatId});
-
+      await db.collection('subscribers').deleteOne({id: chatId});
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
     }
-    if(typeof callback === 'function') {
+    if (typeof callback === 'function') {
       callback();
     }
   }
 
-  async getWeekdays(chat,callback){
-    const client = new MongoClient(mongodb_uri);
+  async getWeekdays(chat, callback) {
+    const client = new MongoClient(mongodbUri);
     try {
       const db = client.db('sv-bot');
       // Insert/update a single document
-      db.collection('subscribers').findOne({ $and: [ {id:chat.id}, {weekdays:{$exists: true}}]},callback);
-
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-
-  }
-
-  async forAllDaily(next){
-    const client = new MongoClient(mongodb_uri);
-    try {
-      const db = client.db('sv-bot');
-      console.log("Connected correctly to server");
-
-      //find all daily subs
-      db.collection('subscribers').find({weekdays:{$exists: false}}).forEach(next);
-
+      db.collection('subscribers').findOne({$and: [{id: chat.id}, {weekdays: {$exists: true}}]}, callback);
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
     }
   }
 
-  async forAllParttime(weekdayIndex, next){
-    const client = new MongoClient(mongodb_uri);
+  async forAllDaily(next) {
+    const client = new MongoClient(mongodbUri);
     try {
       const db = client.db('sv-bot');
-      console.log("Connected correctly to server");
+      console.log('Connected correctly to server');
+
+      // find all daily subs
+      db.collection('subscribers').find({weekdays: {$exists: false}}).forEach(next);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+
+  async forAllParttime(weekdayIndex, next) {
+    const client = new MongoClient(mongodbUri);
+    try {
+      const db = client.db('sv-bot');
+      console.log('Connected correctly to server');
 
       // find all documents containing a given weekday
       db.collection('subscribers').find({weekdays: weekdayIndex}).forEach(next);
-
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
